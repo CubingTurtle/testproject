f8416d8d09b2283a27a1452866552aaaf8b3a5d5
CubingTurtle <115832782+CubingTurtle@users.noreply.github.com>
2023-08-03 18:46:07 -0400

    Update and rename main.java to Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class main {
+class Main {
   public static void main(String []args) {
     Scanner read = new Scanner (System.in);
     System.out.println("Hello user, how are you doing");
